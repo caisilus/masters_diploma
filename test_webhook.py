604175c3df4c69bde91e2c52c15c31602faecf04
caisilus <serangor@gmail.com>
2025-03-15 16:08:25 +0300

    Working wersion of test webhook server

--- test_webhook.py
+++ test_webhook.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI, Request
+import logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
-webhook_app = FastAPI()
+app = FastAPI()
 
-@webhook_app.post("/webhook")
+@app.post("/webhook")
 async def receive_file(request: Request):
     data = await request.form()
     file = data.get("file")
-    print("Получен файл:", file.filename if file else "Нет файла")
+    if file:
+        logger.info(f"GET FILE: {file.filename}")
+    else:
+        logger.error("ERROR: NO FILE")
+
     return {"status": "ok"}
